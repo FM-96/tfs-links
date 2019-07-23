4fb36c014bdc6320e81b1e54598242e09e64e541
Felix Müllner <felix.muellner.96@gmail.com>
2019-07-23 22:09:10 +0200

    Display avatars and tags in action log

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -9,16 +9,22 @@ const actionLog = require('../../utils/action_log.js');
 const version = require('../../utils/version.js');
 
 const Uploader = require('../../models/Uploader.js');
+const User = require('../../models/User.js');
 
 async function actionLogPage(req, res) {
 	const pugData = {
 		auth: req.auth,
 		version,
 		entries: [],
+		users: {},
 	};
 	try {
 		const entries = await actionLog.getEntries();
 		pugData.entries = entries;
+		const usersArr = await User.find({userId: {$in: entries.map(e => e.userId)}}).exec();
+		usersArr.forEach(e => {
+			pugData.users[e.userId] = e;
+		});
 		res.render('action_log', pugData);
 	} catch (err) {
 		logger.error('Error while handling admin page:');
